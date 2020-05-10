69af7e93730d3f212009cab5821c147e16bae4b2
Liz Roten <liz.roten@metc.state.mn.us>
2020-05-10 15:14:28 -0500

    document things_an_architect_should_know

---
+++ R/things_an_architect_should_know.R
@@ -0,0 +1,19 @@
+#' "Two Hundred Fifty Things an Architect Should Know" as a data table.
+#'
+#' A dataset containing all 250 things an architect should know, according to
+#'  [Michael Sorkin](https://en.wikipedia.org/wiki/Michael_Sorkin), an American
+#'  architectural and urban critic, designer, and educator. The list was originally
+#'  published in Sorkin's 2018 book, "What Goes Up: The Rights and Wrongs to a City".
+#'
+#' @format A data frame with 250 rows and 2 variables
+#' \describe{
+#'   \item{n}{int, the order in which the thing was originally published.}
+#'   \item{thing}{character, a thing an architect should know}
+#' }
+#' @docType data
+#'
+#' @details Some things were modified to so that each thing made sense on its own.
+#'     These modifications are in parentheses.
+#'
+#' @source \url{https://www.readingdesign.org/250-things}
+"things_an_architect_should_know"
